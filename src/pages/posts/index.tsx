a5cdfeb8a4cfb5b824354f57c758b2fbdb9be501
neilson <joseneilson25@gmail.com>
2022-03-02 17:44:43 -0300

    install prisma

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Prismic from "@prismicio/client"
+import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss"
 
 export default function Posts(){
@@ -9,13 +12,37 @@ export default function Posts(){
             </Head>
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="">
+                    <a href="#">
                         <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with lerna </strong>
+                        <strong>Creating a Monor epo with lerna </strong>
+                        <p>Creating a Monorepo with lernaCreating a Monorepo with lerna</p>
+                    </a>
+                </div>
+                <div className={styles.posts}>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Creating a Mono repo with lerna </strong>
                         <p>Creating a Monorepo with lernaCreating a Monorepo with lerna</p>
                     </a>
                 </div>
             </main>
         </>
     );
+}
+
+export const getStaticProps: GetStaticProps = async () =>{
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'publixation')
+    ],{
+        fetch: ['publication.title', 'publication.content'],
+        pageSize: 100,
+    })
+
+    console.log(response)
+
+    return{
+        props: {}
+    }
 }
